fad48b8334faa3e74f27b22918a81dd970b042ff
Nickolai Rumiantsev <57686430+NIRumiantsev@users.noreply.github.com>
2020-08-06 11:31:38 +0300

    Update script.js

--- agreement/script.js
+++ agreement/script.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
     if (location.href === "https://internokids.ru/checkout/") {
         $('.form_checkout').ready( function() {
-            console.log($('.checkbox>label').html('Согласен (-а) с условиями <a href="https://internokids.ru/пользовательское-соглашение/" " style="text-decoration: none !important; "><b>Публичной оферты</b></a>'))
+            console.log($('.checkbox>label').html('Согласен (-а) с условиями <a href="https://internokids.ru/terms-of-use/" " style="text-decoration: none !important; "><b>Публичной оферты</b></a>'))
         })
     }
 })
