d1bc3edb91e7fe255197fc6c6f3ed6619d2af5bc
TeemourA <taxprogramming@gmail.com>
2020-08-06 16:28:30 +0300

    fixed agreement.js

--- agreement/script.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function() {
-    if (location.href === "https://internokids.ru/checkout/") {
-        $('.form_checkout').ready( function() {
-            console.log($('.checkbox>label').html('Согласен (-а) с условиями <a /terms-of-use/" " style="text-decoration: none !important; "><b>Публичной оферты</b></a>'))
-        })
-    }
-})
