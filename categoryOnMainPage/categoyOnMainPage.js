800199ce917cc09185e6fe33b65d94275f9aae1c
gafiyatovzz <54131452+gafiyatovzz@users.noreply.github.com>
2020-08-10 22:59:50 +0300

    категории на главной странице

---
+++ categoryOnMainPage/categoyOnMainPage.js
@@ -0,0 +1,62 @@
+document.addEventListener('DOMContentLoaded', function(){
+    
+    const pageSection = document.querySelector('.page-section');
+    const container = document.querySelector('.page-section>.container').cloneNode(true);
+    const products = document.querySelector('.products')
+    const product = products.querySelectorAll('.product-item');
+
+    let twoWeek = document.createElement('div');
+    let oneMonth = document.createElement('div');
+    let row1 = document.createElement('div');
+    let row2 = document.createElement('div');
+    let titleWeek = document.createElement('h2');
+    let titleWeekSpan = document.createElement('span')
+    let titleMonth = document.createElement('h2');
+    let titleMonthSpan = document.createElement('span');
+   
+
+    twoWeek.classList.add('container', 'twoWeek')
+    oneMonth.classList.add('container', 'oneMonth')
+    row1.classList.add('row', 'products', 'grid');
+    row2.classList.add('row', 'products', 'grid');
+    titleWeek.classList.add('section-title', 'mb-4', 'mt-4')
+    titleMonth.classList.add('section-title', 'mb-4', 'mt-4')
+
+    titleWeek.textContent = 'Двухнедельные линзы';
+    titleMonth.textContent = 'Линзы месячного ношения';
+
+    pageSection.append(twoWeek);
+    pageSection.append(oneMonth); 
+
+    titleWeekSpan.textContent = 'Двухнедельные линзы';
+    titleMonthSpan.textContent = 'Линзы месячного ношения';
+
+    const monthCont = document.querySelector('.oneMonth');
+    const weekCont = document.querySelector('.twoWeek');
+
+    monthCont.append(row1);
+    weekCont.append(row2);
+ 
+    weekCont.prepend(titleWeek);
+    monthCont.prepend(titleMonth);
+    titleWeek.append(titleWeekSpan);
+    titleMonth.append(titleMonthSpan);
+    
+    const monthRow = document.querySelector('.oneMonth>.products');
+    const weekRow = document.querySelector('.twoWeek>.products');
+
+    const monthArr = ['688', '687', '669', '977']
+    const weekArr = ['668', '667']
+  
+    product.forEach(function (i){
+        const attr = i.getAttribute('data-product_id')
+    
+        if (attr === '688' || attr === '687' || attr === '669' || attr === '977') {
+            monthRow.append(i);
+        } 
+        if (attr === '668' || attr === '667') {
+            weekRow.append(i);
+        }
+    })
+
+})
