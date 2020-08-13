3fc34c4ab34a678678d95ce8d11c4f3a3620e073
TeemourA <taxprogramming@gmail.com>
2020-08-13 17:16:58 +0300

    swap async ajax to sync

--- VUE-COMPONENTS/CategoriesList/CategoriesList.js
+++ VUE-COMPONENTS/CategoriesList/CategoriesList.js
@@ -2,7 +2,7 @@ $(document).ready(function () {
 
 
   const categoriesArr = Array.from(document.querySelectorAll(".nav .sf-menu > li"));
-  const categoriesTrimmed = categoriesArr.slice(1, categoriesArr.length - 1);
+  const categoriesTrimmed = categoriesArr.slice(1, categoriesArr.length);
 
   const categoriesLinks = categoriesTrimmed.map((li) => {
     const link = li.firstElementChild.href;
@@ -13,27 +13,34 @@ $(document).ready(function () {
   const categories = categoriesLinks.map((link) => {
     const result = {};
 
-    $.get(link, function (data) {
-      const doc = new DOMParser().parseFromString(data, "text/html");
+    $.ajax({
+      url: link,
+      type: "get",
+      async: false,
+      success: function (data) {
+        const doc = new DOMParser().parseFromString(data, "text/html");
 
-      const categoryTitle = doc.querySelector('.page-header h1').textContent;
-      const categoryLink = link;
-      let imgLink;
+        const categoryTitle = doc.querySelector('.page-header h1').textContent;
+        const categoryLink = link;
+        let imgLink;
 
-      if (doc.querySelector('.main-slider .sub img')) {
-        imgLink = doc.querySelector('.main-slider .sub img').src;
-      } else {
-        imgLink = 'https://via.placeholder.com/350x350';
-      }
+        if (doc.querySelector('.main-slider .sub img')) {
+          imgLink = doc.querySelector('.main-slider .sub img').src;
+        } else {
+          imgLink = 'https://via.placeholder.com/350x350';
+        }
 
-      result.title = categoryTitle;
-      result.link = categoryLink;
-      result.img = imgLink;
+        result.title = categoryTitle;
+        result.link = categoryLink;
+        result.img = imgLink;
+      },
     });
 
     return result;
 
   });
+  
+  console.log(categories);
 
 
   const categoriesContainer = document.querySelector("#home > div.wrapper.wrapper-closed > div.content-area > section.page-section.homefeatured_category > div");
@@ -45,17 +52,19 @@ $(document).ready(function () {
     listDiv.setAttribute('id', 'app');
     categoriesContainer.append(listDiv);
 
+    const cat = [...categories];
+
     const categoriesList = new Vue({
       el: '#app',
       data: {
-        categories: categories
+        categories: cat
       },
       template: `
                  <div class="row">
                    <div class="custom-categories__item" v-for="category of categories">
-                     <a href="category.link">
+                     <a :href="category.link">
                        <span class="custom-categories__title-container"><h4 class="custom-categories__title">{{ category.title }}</h4></span>
-                       <img :src="category.imgURL"></img>
+                       <img :src="category.img"></img>
                      </a> 
                    </div>
                  </div>
