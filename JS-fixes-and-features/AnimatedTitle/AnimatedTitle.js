24fb9d67a3b9e9536955f8fac4bf1b7048f4bf21
truehusla <nirumiantsev@gmail.com>
2020-08-21 17:24:27 +0300

    Fix initialisation scroll position

--- JS-fixes-and-features/AnimatedTitle/AnimatedTitle.js
+++ JS-fixes-and-features/AnimatedTitle/AnimatedTitle.js
@@ -2,7 +2,7 @@
 window.onscroll = function() {myFunction()};
 
 function myFunction() {
-    if (document.body.scrollTop > 550 || document.documentElement.scrollTop > 550) {
+    if (document.body.scrollTop > 350 || document.documentElement.scrollTop > 350) {
         document.querySelector(".page-section > .container > .section-title").classList.add("section-title--animated");
     }
 }
