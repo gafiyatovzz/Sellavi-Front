04e6cc704c79f979c27bae983d63525f23a9988c
gafiyatovzz <54131452+gafiyatovzz@users.noreply.github.com>
2020-08-27 18:19:23 +0300

    a

--- JS-fixes-and-features/requsitesZak/requsites.js
+++ JS-fixes-and-features/requsitesZak/requsites.js
@@ -11,27 +11,48 @@ const CORR_NUMBER = '';
 const BIK = '';
 const DIRECTOR = 'Когай Жанна Анатольевна';
 
-if(location.href.includes('terms-of-use')) {
-    let requsitesData = $('<div class="requsites-data"></div>'),
-        requsitesText = $('<div class="requsites__data">' +
-            `<h4 class="requsites__title">Реквизиты продавца</h4>` +
-            `<p class="requsites__elem">Название: <span>${ORG_NAME}</span></p>` +
-            `<p class="requsites__elem">Юр.адрес: <span>${ORG_ADRRESS}</span></p>` +
-            `<p class="requsites__elem">Инн: <span>${INN}</span></p>` +
-            `<p class="requsites__elem">КПП: <span>${KPP}</span></p>` +
-            `<p class="requsites__elem">ОГРН: <span>${OGRN}</span></p>` +
-            `<p class="requsites__elem">Банк: <span>${BANKNAME}</span></p>` +
-            `<p class="requsites__elem">Р/С: <span>${ACCOUT_NUMBER}</span></p>` +
-            `<p class="requsites__elem">К/С: <span>${CORR_NUMBER}</span></p>` +
-            `<p class="requsites__elem">БИК: <span>${BIK}</span></p>` +
-            `<p class="requsites__elem">Ген.дир: <span>${DIRECTOR}</span></p>` +
-        '</div>');
-    console.log(requsitesText);
-    $('.requsitess').append(requsitesData);
-    $('.requsites-data').append(requsitesText)
+
+if (location.href.includes('terms-of-use')) {
+    const root = document.querySelector('.requsitess');
+    let requsitesData = document.createElement('div');
+        requsitesData.classList.add('.requsites-data');
+        requsitesData.id = 'requisites';
+    if(root) {
+      root.append(requsitesData);
+      let Requisites = new Vue({
+          el: '#requisites',
+          data: {
+              requisites: [
+                ORG_NAME,
+                ORG_ADRRESS,
+                INN,
+                KPP,
+                OGRN,
+                BANKNAME,
+                ACCOUT_NUMBER,
+                CORR_NUMBER,
+                BIK
+              ],
+          },
+          template: `
+                <div class="requsites__data">
+                    <h4 class="requsites__title">Реквизиты продавца</h4>
+                    <p class="requsites__elem">Название: <span>${ORG_NAME}</span></p>
+                    <p class="requsites__elem">Юр.адрес: <span>${ORG_ADRRESS}</span></p>
+                    <p class="requsites__elem">Инн: <span>${INN}</span></p>
+                    <p class="requsites__elem">КПП: <span>${KPP}</span></p>
+                    <p class="requsites__elem">ОГРН: <span>${OGRN}</span></p>
+                    <p class="requsites__elem">Банк: <span>${BANKNAME}</span></p>
+                    <p class="requsites__elem">Р/С: <span>${ACCOUT_NUMBER}</span></p>
+                    <p class="requsites__elem">К/С: <span>${CORR_NUMBER}</span></p>
+                    <p class="requsites__elem">БИК: <span>${BIK}</span></p>
+                    <p class="requsites__elem">Ген.дир: <span>${DIRECTOR}</span></p>
+                </div>`
+        })
+    } else console.log('No element with id:list');
 }
 
-if (!location.href.includes('checkout')) {
+if (location.href.includes('contact')) {
   const root = document.querySelector('.contact-info');
   const list = document.createElement('div');
   list.id = 'requisites';
