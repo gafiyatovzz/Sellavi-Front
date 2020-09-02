fa6428fce0591aa065fc817466076868cd4e2a7b
Тимур Ахмедов <timurahmedov@MacBook-Air-Timur.local>
2020-09-02 12:46:47 +0300

    fix tabulation

--- JS-fixes-and-features/js/translate-about-us.js
+++ JS-fixes-and-features/js/translate-about-us.js
@@ -1,16 +1,16 @@
 /* Translate text */
-$(document).ready(function() {
-      
-   const widget = $('.footer-widgets>.container>.row>.col-md-3:first>.widget')
-   const widTitle = widget.find('.widget-title');
-   const widCat = widget.find('.widget-categories>ul>li:first-child>a');
-   let textTrans = 'About us ';
-   let textRus = 'О нас'
+$(document).ready(function () {
 
-   if (widTitle.html() == textTrans || widCat.html() == textTrans) {
-      widTitle.text(textRus);
-      widCat.text(textRus);
-    }
+  const widget = $('.footer-widgets>.container>.row>.col-md-3:first>.widget')
+  const widTitle = widget.find('.widget-title');
+  const widCat = widget.find('.widget-categories>ul>li:first-child>a');
+  let textTrans = 'About us ';
+  let textRus = 'О нас'
+
+  if (widTitle.html() == textTrans || widCat.html() == textTrans) {
+    widTitle.text(textRus);
+    widCat.text(textRus);
+  }
 });
 
 // /* Change policy text DELETE */
