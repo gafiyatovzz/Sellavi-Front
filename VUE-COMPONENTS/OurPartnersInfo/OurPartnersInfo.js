fe4530ae1cbe4b4fdc66a7de080ca4003018bc65
truehusla <nirumiantsev@gmail.com>
2020-09-07 10:21:08 +0300

    Add the main component logic and basic styles

--- VUE-COMPONENTS/OurPartnersInfo/OurPartnersInfo.js
+++ VUE-COMPONENTS/OurPartnersInfo/OurPartnersInfo.js
@@ -10,21 +10,45 @@ $(document).ready(() => {
       el: '#partners',
       data: {
         partnersLogos: [
-          'https://lh4.googleusercontent.com/uTPcQFh5Yte0UdrQEGeuHGSD6tf4pOq_clcoAGkeZKn2gK4vqP7sZFq_pbuBKO4ftHCpe_VDGVaNoZAXZ4fD8-nLFR8azWn-miyo-EpbAcDBBM664tsSXgQd2SrNJYPB9F2RSkf6QvBDVnMjvA',
-          'https://lh4.googleusercontent.com/uTPcQFh5Yte0UdrQEGeuHGSD6tf4pOq_clcoAGkeZKn2gK4vqP7sZFq_pbuBKO4ftHCpe_VDGVaNoZAXZ4fD8-nLFR8azWn-miyo-EpbAcDBBM664tsSXgQd2SrNJYPB9F2RSkf6QvBDVnMjvA',
-          'https://lh4.googleusercontent.com/uTPcQFh5Yte0UdrQEGeuHGSD6tf4pOq_clcoAGkeZKn2gK4vqP7sZFq_pbuBKO4ftHCpe_VDGVaNoZAXZ4fD8-nLFR8azWn-miyo-EpbAcDBBM664tsSXgQd2SrNJYPB9F2RSkf6QvBDVnMjvA',
-          'https://lh4.googleusercontent.com/uTPcQFh5Yte0UdrQEGeuHGSD6tf4pOq_clcoAGkeZKn2gK4vqP7sZFq_pbuBKO4ftHCpe_VDGVaNoZAXZ4fD8-nLFR8azWn-miyo-EpbAcDBBM664tsSXgQd2SrNJYPB9F2RSkf6QvBDVnMjvA',
-          'https://lh4.googleusercontent.com/uTPcQFh5Yte0UdrQEGeuHGSD6tf4pOq_clcoAGkeZKn2gK4vqP7sZFq_pbuBKO4ftHCpe_VDGVaNoZAXZ4fD8-nLFR8azWn-miyo-EpbAcDBBM664tsSXgQd2SrNJYPB9F2RSkf6QvBDVnMjvA',
-          'https://lh4.googleusercontent.com/uTPcQFh5Yte0UdrQEGeuHGSD6tf4pOq_clcoAGkeZKn2gK4vqP7sZFq_pbuBKO4ftHCpe_VDGVaNoZAXZ4fD8-nLFR8azWn-miyo-EpbAcDBBM664tsSXgQd2SrNJYPB9F2RSkf6QvBDVnMjvA',    'https://lh4.googleusercontent.com/uTPcQFh5Yte0UdrQEGeuHGSD6tf4pOq_clcoAGkeZKn2gK4vqP7sZFq_pbuBKO4ftHCpe_VDGVaNoZAXZ4fD8-nLFR8azWn-miyo-EpbAcDBBM664tsSXgQd2SrNJYPB9F2RSkf6QvBDVnMjvA',
-          'https://lh4.googleusercontent.com/uTPcQFh5Yte0UdrQEGeuHGSD6tf4pOq_clcoAGkeZKn2gK4vqP7sZFq_pbuBKO4ftHCpe_VDGVaNoZAXZ4fD8-nLFR8azWn-miyo-EpbAcDBBM664tsSXgQd2SrNJYPB9F2RSkf6QvBDVnMjvA',
-          'https://lh4.googleusercontent.com/uTPcQFh5Yte0UdrQEGeuHGSD6tf4pOq_clcoAGkeZKn2gK4vqP7sZFq_pbuBKO4ftHCpe_VDGVaNoZAXZ4fD8-nLFR8azWn-miyo-EpbAcDBBM664tsSXgQd2SrNJYPB9F2RSkf6QvBDVnMjvA',
-        ]
+          'https://www.sberindex.ru/images/favicon/icon200.png',
+          'https://brobank.ru/wp-content/uploads/2019/10/vtb_logo_icon.png',
+          'https://www.gazprom.ru/f/1/gazprom-logo-ru.png',
+          'https://upload.wikimedia.org/wikipedia/commons/thumb/f/ff/LUK_OIL_Logo.svg/1200px-LUK_OIL_Logo.svg.png',
+          'https://www.sberindex.ru/images/favicon/icon200.png',
+          'https://brobank.ru/wp-content/uploads/2019/10/vtb_logo_icon.png',
+          'https://www.gazprom.ru/f/1/gazprom-logo-ru.png',
+          'https://upload.wikimedia.org/wikipedia/commons/thumb/f/ff/LUK_OIL_Logo.svg/1200px-LUK_OIL_Logo.svg.png',
+        ],
+        activePartners: [
+          'https://www.sberindex.ru/images/favicon/icon200.png',
+          'https://brobank.ru/wp-content/uploads/2019/10/vtb_logo_icon.png',
+          'https://www.gazprom.ru/f/1/gazprom-logo-ru.png',
+        ],
+        startIndex: 0,
+        partnersToShow: 3,
+      },
+      methods: {
+        changeActive () {
+          setInterval(() => {
+            this.activePartners = [];
+          }, 4999);
+          setInterval(() => {
+            this.startIndex += this.partnersToShow;
+            if (this.startIndex >= this.partnersLogos.length) {
+              this.startIndex = 0;
+            }
+            this.activePartners = this.partnersLogos.slice(this.startIndex, this.startIndex + this.partnersToShow);
+          }, 5000)
+        }
+      },
+      mounted() {
+        this.changeActive()
       },
       template: `
           <div class="container partners-info__container">
             <h4 class="section-title partners-info__title">Наши партнеры</h4>
             <ul class="partners-info__list">
-              <li class="partners-info__item" v-for="logo of partnersLogos">
+              <li class="partners-info__item" v-for="logo of activePartners">
                 <img class="partners-info__img" :src="logo">
               </li>
             </ul>
