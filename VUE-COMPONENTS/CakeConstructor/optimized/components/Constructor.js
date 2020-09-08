bb2dc89ec4b3ed49e1624d920cbe872de5dfe917
truehusla <nirumiantsev@gmail.com>
2020-09-08 21:49:42 +0300

    Add the main logic

---
+++ VUE-COMPONENTS/CakeConstructor/optimized/components/Constructor.js
@@ -0,0 +1,98 @@
+document.addEventListener('DOMContentLoaded', () => {
+    return (Vue.component('constructor', {
+        props: ['price', 'tiersStructures', 'tastes', 'fillings', 'selectedProperties', 'forms', 'covers', 'toppings', 'berries', 'berriePrice', 'decors', 'decorPrice', 'activeSelectors'],
+        methods: {
+            selectProperty(name, value){
+                this.$emit('select-property', name, value);
+            },
+            activateSelector(name){
+                this.$emit('activate-selector', name);
+            },
+            addFeatures(event, name){
+                this.$emit('add-features', event, name);
+            },
+            saveAndBuy() {
+                this.$emit('save-and-buy')
+            }
+        },
+        template: `
+                <div class="constructor">
+                    <h2 class="constructor__title">Собери свой собственный торт!</h2>
+                    <div class="constructor__container">
+                        <div class="constructor__panel">
+                            <div class="constructor__selectors">
+                                <selector :active-selectors="activeSelectors"
+                                    :name="'tires'"
+                                    :title="'число слоёв'"
+                                    :options="tiersStructures"
+                                    :selected-properties="selectedProperties"
+                                    @select-property="selectProperty"
+                                    @activate-selector="activateSelector"
+                                ></selector>
+                                <div class="constructor__extra-options" v-if="selectedProperties.tires">
+                                <div class="constructor__selector-group" v-for="n in selectedProperties.tires">
+                                    <selector :active-selectors="activeSelectors"
+                                        :name="'taste'+n"
+                                        :title="'вкус яруса '+n"
+                                        :options="tastes"
+                                        :selected-properties="selectedProperties"
+                                        @select-property="selectProperty"
+                                        @activate-selector="activateSelector"
+                                    ></selector>
+                                    <selector :active-selectors="activeSelectors"
+                                        :name="'filling'+n"
+                                        :title="'начинка яруса '+n"
+                                        :options="fillings"
+                                        :selected-properties="selectedProperties"
+                                        @select-property="selectProperty"
+                                        @activate-selector="activateSelector"
+                                    ></selector>
+                                </div>
+                            </div>
+                                <selector :active-selectors="activeSelectors"
+                                :name="'form'"
+                                :title="'форма торта'"
+                                :options="forms"
+                                :selected-properties="selectedProperties"
+                                @select-property="selectProperty"
+                                @activate-selector="activateSelector"
+                            ></selector>
+                                <selector :active-selectors="activeSelectors"
+                                :name="'cover'"
+                                :title="'покрытие торта'"
+                                :options="covers"
+                                :selected-properties="selectedProperties"
+                                @select-property="selectProperty"
+                                @activate-selector="activateSelector"
+                            ></selector>
+                                <selector :active-selectors="activeSelectors"
+                                :name="'topping'"
+                                :title="'топпинг'"
+                                :options="toppings"
+                                :selected-properties="selectedProperties"
+                                @select-property="selectProperty"
+                                @activate-selector="activateSelector"
+                            ></selector>
+                            </div>
+                            
+                            <check-list :options="berries"
+                               :title="'ягоды для украшения'"
+                               :name="'berries'"
+                               @add-features="addFeatures"
+                            ></check-list>
+                            <check-list :options="decors"
+                               :title="'декор'"
+                               :name="'decor'"
+                               @add-features="addFeatures"
+                            ></check-list>
+                        </div>
+                        <div class="constructor__window">
+            
+                        </div>
+                        <button @click="saveAndBuy">Заказать торт!</button>
+                    </div>
+                </div>
+            `
+
+    }))
+});
