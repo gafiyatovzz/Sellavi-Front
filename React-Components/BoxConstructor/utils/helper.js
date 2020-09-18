8566a7159463fb41705078b0a5205509aa0e8842
truehusla <nirumiantsev@gmail.com>
2020-09-18 10:32:16 +0300

    Add box constructor basic structure

---
+++ React-Components/BoxConstructor/utils/helper.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const root = document.createElement('div');
+  root.setAttribute('id', 'root');
+
+  document.querySelector(".custom_section").append(root);
+});
